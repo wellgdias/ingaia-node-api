f1b683e144fbb35a60a7b05f4d9dfdc926051883
Wellington Dias <wads90@gmail.com>
2020-08-11 22:33:46 -0300

    initial setup

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**'
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
+  transform: { '.+\\.ts$': 'ts-jest' }
+}
