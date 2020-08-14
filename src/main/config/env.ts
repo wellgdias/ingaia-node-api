cb34059449870119484b920043daa4ad8d5dba4c
Wellington Dias <wads90@gmail.com>
2020-08-14 00:17:09 -0300

    add dotenv

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,7 +1,9 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 export default {
-  mongoUrl:
-    process.env.MONGO_URL || 'mongodb://mongo:27017/ingaia-node-api',
-  port: process.env.PORT || 3030,
+  mongoUrl: process.env.MONGO_URL,
+  port: process.env.PORT,
   weatherApi: {
     Url: 'https://api.openweathermap.org/data/2.5',
     Key: process.env.WEATHER_APIKEY
