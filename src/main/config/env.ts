1988c7842dac919407934fe633b1f785e6b2069e
Wellington Dias <wads90@gmail.com>
2020-08-13 23:30:48 -0300

    add docker-compose

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,6 @@
 export default {
   mongoUrl:
-    process.env.MONGO_URL || 'mongodb://localhost:27017/ingaia-node-api',
+    process.env.MONGO_URL || 'mongodb://mongo:27017/ingaia-node-api',
   port: process.env.PORT || 3030,
   weatherApi: {
     Url: 'https://api.openweathermap.org/data/2.5',
